3a204d51f1e78516b504568b8d39875a550ba3cf
Donald Newholm <newholm1@hotmail.com>
2019-12-24 15:34:23 +0000

    Refactor tests ready to add new features

--- lib/van.rb
+++ lib/van.rb
@@ -1,7 +1,14 @@
 class Van
+  attr_reader :bikes
+  
+  def initialize
+    @bikes = []
+  end
 
-  def collect_bikes(origin)
-    "Broken bikes collected"
+  def collect(bike, origin)
+    fail "This bike does not need to be repaired" if bike.working? # && origin.class == DockingStation
+    @bikes << bike
+    @bikes.last
   end
 
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,15 +1,27 @@
 require 'van'
 
 describe Van do 
-  it 'responds to collect_bikes' do
-    expect(subject).to respond_to(:collect_bikes)
+  it 'responds to collect' do
+    expect(subject).to respond_to(:collect)
   end
 
-  describe '#collect_bikes' do
+  describe '#collect' do
     let(:docking_station) { double :docking_station }
     let(:bike) { double :bike }
-    it 'collects a bike' do
-      expect(subject.collect_bikes(docking_station)).to eq("Broken bikes collected")
+
+    it 'collects a broken bike from a docking station' do
+      allow(bike).to receive(:working?).and_return(false)
+      allow(docking_station).to receive(:dock).and_return(bike)
+      docking_station.dock(bike)
+      expect(subject.collect(bike, docking_station)).to eq(bike)
     end
+
+    it 'does not collect a working bike from a docking station' do
+      allow(bike).to receive(:working?).and_return(true)
+      allow(docking_station).to receive(:dock).and_return(bike)
+      docking_station.dock(bike)
+      expect { subject.collect(bike, docking_station) }.to raise_error("This bike does not need to be repaired")
+    end
+
   end 
 end
